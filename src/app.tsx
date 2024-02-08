14587abcf3826a394298a01e5f99241db055fe05
gabriel-roque <gabrielrms.dll@gmail.com>
2024-02-08 12:13:19 -0300

    📦 feat: delete note

--- src/app.tsx
+++ src/app.tsx
@@ -36,6 +36,15 @@ export function App() {
     setSearch(query);
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotes(notesArray);
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+  }
+
   const filteredNotes =
     search !== ''
       ? notes.filter((note) =>
@@ -44,7 +53,7 @@ export function App() {
       : notes;
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -58,10 +67,10 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map((note) => (
-          <NoteCard note={note} key={note.id} />
+          <NoteCard note={note} key={note.id} onNoteDeleted={onNoteDeleted} />
         ))}
       </div>
     </div>
